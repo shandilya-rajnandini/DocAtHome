dd6babbaf768522772a7d14766abacbc82c713b5
Tarana Khanna <taranakhanna5@gmail.com>
2025-07-31 20:02:19 +0530

    resolved conflict

--- backend/tests/auth.test.js
+++ backend/tests/auth.test.js
@@ -4,7 +4,7 @@ const app = require('../app');
 const User = require('../models/User');
 
 beforeAll(async () => {
-    await mongoose.connect(process.env.MONGODB_URI, {
+    await mongoose.connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true
     })
