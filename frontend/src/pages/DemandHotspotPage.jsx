215a0c89ef237ed5d04e1d6e08fdbfb917c4608c
Tarana Khanna <taranakhanna5@gmail.com>
2025-08-17 13:13:44 +0530

    fix: import for useAuthStore

--- frontend/src/pages/DemandHotspotPage.jsx
+++ frontend/src/pages/DemandHotspotPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
-import useAuthStore from '../store/authStore';
+import useAuthStore from '../store/useAuthStore';
 
 const DemandHotspotPage = () => {
     const { user } = useAuthStore();
